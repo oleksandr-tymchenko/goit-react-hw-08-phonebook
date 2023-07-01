2051a031752dc224677e3bbc484dc77fc1e9e68e
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-07-01 13:49:59 +0300

    start

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import { App } from 'components/App';
 import './index.css';
 import { Global, ThemeProvider } from '@emotion/react';
 import { GlobalStyles, theme } from 'styles';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyles} />
       <Provider store={store}>
-        <BrowserRouter basename="goit-react-hw-08-phonebook">
-          <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
