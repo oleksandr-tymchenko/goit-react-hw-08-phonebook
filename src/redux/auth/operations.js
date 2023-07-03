a0c318b2aafc2e4fa760d4d3c04aaf2f975e1ebe
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-07-03 22:09:52 +0300

    fixed auth operations

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -58,7 +58,7 @@ export const refreshUser = createAsyncThunk(
 
     try {
       setAuthHeader(token);
-      const res = await axios.get('/users/me');
+      const res = await axios.get('/users/current');
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
